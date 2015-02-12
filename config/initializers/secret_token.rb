8ed1d31b26d9341ca6c51a24490f046e8cc6cb8a
vanzalden <tilovanzalden@t-online.de>
2015-02-12 12:02:02 +0100

    employees

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Employees::Application.config.secret_token = 'c4d3d3427e37c4a22e617bc2b117c087a7004ca6740fee4471c2a85e7bd922dd41e949202845254c21d8893ed0b1a264f56e18bd327e8107068283048589ee62'
